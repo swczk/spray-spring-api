4951cdd954262f52d09293ff3c3468a0384ac977
Eduardo Fadel <1eduardofadel@gmail.com>
2025-04-10 10:04:19 -0300

    Aula 2 Neon

--- src/main/java/br/edu/utfpr/api1/controller/PessoaController.java
+++ src/main/java/br/edu/utfpr/api1/controller/PessoaController.java
@@ -1,19 +1,31 @@
 package br.edu.utfpr.api1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.utfpr.api1.model.Pessoa;
+import br.edu.utfpr.api1.repository.PessoaRepository;
 
 @RestController
 @RequestMapping(value = "/pessoa", produces = "application/json")
 public class PessoaController {
+    @Autowired
+    private PessoaRepository pessoaRepository;
     
     @GetMapping("/1")
     public Pessoa getOne(){
         var p = new Pessoa(1, "eduardo", "1eduardofadel@gmail.com");
         return p;
+    }
+
+    @PostMapping
+    public Pessoa create (@RequestBody Pessoa p) {
+        return pessoaRepository.save(p);    
+    
     }
 
 }
