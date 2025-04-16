ea63b47c40d6eeec53ffc0d0b9452aa7ccd1c2c6
Pedro Sawczuk <89877096+swczk@users.noreply.github.com>
2025-04-16 09:42:06 -0300

    fix: added Serializable to BaseEntity

--- src/main/java/br/edu/utfpr/api1/model/BaseEntity.java
+++ src/main/java/br/edu/utfpr/api1/model/BaseEntity.java
@@ -9,20 +9,24 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 @Data
 @MappedSuperclass
-public abstract class BaseEntity {
-
+public abstract class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @CreationTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
