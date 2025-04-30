1c30cf2868022b5c9965b5f0b8c20b4d9cafb713
Pedro Sawczuk <89877096+swczk@users.noreply.github.com>
2025-04-30 09:57:38 -0300

    .

--- src/main/java/br/edu/utfpr/api1/dto/AplicacaoStatusDTO.java
+++ src/main/java/br/edu/utfpr/api1/dto/AplicacaoStatusDTO.java
@@ -1,12 +1,14 @@
 package br.edu.utfpr.api1.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class AplicacaoStatusDTO {
-    @NotBlank(message = "Campo 'finalizada' é obrigatório")
+    @NotNull(message = "Campo 'finalizada' é obrigatório")
     private Boolean finalizada;
 }
--- src/main/java/br/edu/utfpr/api1/dto/UsuarioStatusDto.java
+++ src/main/java/br/edu/utfpr/api1/dto/UsuarioStatusDto.java
@@ -1,12 +1,14 @@
 package br.edu.utfpr.api1.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class UsuarioStatusDto {
-    @NotBlank(message = "Campo 'ativo' é obrigatório")
+    @NotNull(message = "Campo 'ativo' é obrigatório")
     private Boolean ativo;
 }
